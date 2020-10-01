98d9cf177173d31089ade2066d7ca90916bcf645
Wojtek <wojciech.brzuszkiewicz@hotmail.com>
2020-10-01 11:36:36 +0200

    Globals fix

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ import os
 # data.server_name, data.address, data.max_players, data.max_count, data.port
 # TOKEN, SECRET_KEY
 
+master_servers = {};
+
 app = Flask(__name__);
 app.register_blueprint(game_service_view);
 
 def register_master_servers():
-    globals.master_servers['QuickHand'] = ServerHandler(10);
+    master_servers['QuickHand'] = ServerHandler(10);
 
-    for key, master_server in globals.master_servers.items():
+    for key, master_server in master_servers.items():
         master_server.start();
 
 def run():
